4ad9b1bc2d1391c6cc4b92e4c0cf63baa9d5ab10
Johann <johann.meyer@gmail.com>
2018-01-06 11:19:12 +0200

    Initial Commit

---
+++ particle-filter-tracker/kullback_lieber_divergence.py
@@ -0,0 +1,20 @@
+import numpy as np
+""" Description of what KL divergence is. """
+
+def discrete_kl_divergence(P, Q):
+    """ Calculates the Kullback-Lieber divergence
+    according to the discrete definition:
+    sum [P(i)*log[P(i)/Q(i)]]
+    where P(i) and Q(i) are discrete probability
+    distributions. In this case the one """
+
+    """ Epsilon is used here to avoid conditional code for
+    checking that neither P or Q is equal to 0. """
+    epsilon = 0.00001
+
+    # To avoid changing the color model, a copy is made
+    temp_P = P+epsilon
+    temp_Q = Q+epsilon
+
+    divergence=np.sum(temp_P*np.log(temp_P/temp_Q))
+    return divergence
